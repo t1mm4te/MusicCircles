27304ca96fa9322d88f5c553bf71644cd05d6825
Tim Igl <timofka6@gmail.com>
2025-02-06 23:44:43 +0700

    Первый код для бота, пока еле рабочий, и лишь отдаленно представляющий возможности бота

---
+++ main.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+import logging
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (
+    Application,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+)
+
+load_dotenv("config/config.env")
+
+TOKEN = os.getenv("TB_TOKEN")
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+def main() -> None:
+    application = Application.builder().token()
+
